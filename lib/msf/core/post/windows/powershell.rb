2b76fb2717b391bb41b14f1c9606a8de4a5289f0
Carlos Perez <carlos_perez@darkoperator.com>
2012-09-04 18:38:47 -0400

    Changes to PowerShell Module and Mixin
    Modified mixin with improve cleanup
    Removed write to log function
    Modified cleanup call
    Added powershell script for enabling remoting
    modified Post Module to use changed mixin and save to loot

--- lib/msf/core/post/windows/powershell.rb
+++ lib/msf/core/post/windows/powershell.rb
@@ -114,25 +114,58 @@ module Powershell
 	# to disk.
 	#
 	def execute_script(script, time_out = 15)
-		running_pids, open_channels = [], []
-		# Execute using -EncodedCommand
-		session.response_timeout = time_out
-		cmd_out = session.sys.process.execute("powershell -EncodedCommand " +
-			"#{script}", nil, {'Hidden' => true, 'Channelized' => true})
+		psh_pid = nil
+		pids = []
+		cmd_out = ""
+		begin
+			::Timeout::timeout(time_out) do
+				# Execute scriot
+				psh_process = session.sys.process.execute("powershell -EncodedCommand  " +
+						"#{script}", nil, {'Hidden' => true, 'Channelized' => true})
 
-		# Add to list of running processes
-		running_pids << cmd_out.pid
+				# Save the PID of the process to kill it and any child process if timeout or hang
+				psh_pid = psh_process.pid
 
-		# Add to list of open channels
-		open_channels << cmd_out
+				# Read the channel output
+				while (channel = psh_process.channel.read)
+					cmd_out << channel
+				end
 
-		return [cmd_out, running_pids, open_channels]
+				# Close channel
+				psh_process.channel.close
+
+				#Close the process
+				psh_process.close
+			end
+
+		# Do cleanup if the script hangs or runs for to long. Removes any child process
+		# that could have been created
+		rescue Timeout::Error
+			# Finding PIDs of processes created
+			session.sys.process.processes.each do |proc|
+				pids << proc['pid'] if (proc["ppid"] == psh_pid.to_i)
+			end
+			if pids.length >> 0
+				# add original process pid to the list
+				pids << psh_pid
+
+				# terminating processes
+				pids.each do |pid|
+					session.sys.process.kill(pid)
+				end
+			else
+				session.sys.process.kill(psh_pid)
+			end
+			# Raise timeout error after cleanup so it is know a timeout happened
+			raise Timeout::Error
+		end
+		return cmd_out
 	end
 
 
 	#
-	# Powershell scripts that are longer than 8000 bytes are split into 8000 
-	# 8000 byte chunks and stored as environment variables. A new powershell 
+	# Powershell scripts that are longer than 8000 bytes are split into 8000
+	# 8000 byte chunks and stored as environment variables. A new powershell
 	# script is built that will reassemble the chunks and execute the script.
 	# Returns the reassembly script.
 	#
@@ -183,57 +216,19 @@ module Powershell
 		return encoded_script
 	end
 
-	#
-	# Log the results of the powershell script
-	#
-	def write_to_log(cmd_out, log_file, eof)
-		# Open log file for writing
-		fd = ::File.new(log_file, 'w+')
-
-		# Read output until eof and write to log
-		while (line = cmd_out.channel.read())
-			if (line.sub!(/#{eof}/, ''))
-				fd.write(line)
-				vprint_good("\t#{line}")
-				cmd_out.channel.close()
-				break
-			end
-			fd.write(line)
-			vprint_good("\t#{line}")
-		end
-
-		# Close log file
-		fd.close()
-
-		return
-	end
 
 	#
-	# Clean up powershell script including process and chunks stored in environment variables
+	# Clean up powershell script for chunks stored in environment variables
 	#
-	def clean_up(script_file = nil, eof = '', running_pids =[], open_channels = [], env_suffix = Rex::Text.rand_text_alpha(8), delete = false)
+	def clean_up(env_suffix)
 		# Remove environment variables
 		env_del_command =  "[Environment]::GetEnvironmentVariables('User').keys|"
 		env_del_command += "Select-String #{env_suffix}|%{"
 		env_del_command += "[Environment]::SetEnvironmentVariable($_,$null,'User')}"
 		script = compress_script(env_del_command, eof)
-		cmd_out, running_pids, open_channels = *execute_script(script)
-		write_to_log(cmd_out, "/dev/null", eof)
+		cmd_out = execute_script(script)
 
-		# Kill running processes
-		running_pids.each() do |pid|
-			session.sys.process.kill(pid)
-		end
-
-
-		# Close open channels
-		open_channels.each() do |chan|
-			chan.channel.close()
-		end
-
-		::File.delete(script_file) if (script_file and delete)
-
-		return
+		return cmd_out
 	end
 
 end
